3e5d6996bad0776b0f86e8d1b573c9a8923930df
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-03-08 11:51:09 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+
+const db = mongoose.connection
+db.on('error', err => {
+  console.log(err)
+})
+
+db.on('open', () => {
+  console.log('Connected to MongoDB')
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT)
+})
